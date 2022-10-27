19243a217074e9c69644b48e377b2e4003ab3602
qazmko10 <nikitatslnk@gmail.com>
2022-10-27 19:12:12 +0300

    final commit

---
+++ basic-2/src/task_4/Car.java
@@ -0,0 +1,29 @@
+package task_4;
+
+public class Car {
+    int year;
+    double speed;
+    int weight;
+    String color;
+
+    public Car() {}
+
+    public Car(int year) {
+        this.year = year;
+    }
+
+    public Car(int year, double speed) {
+        this(year);
+        this.speed = speed;
+    }
+
+    public Car(int year, double speed, int weight) {
+        this(year, speed);
+        this.weight = weight;
+    }
+
+    public Car(int year, double speed, int weight, String color) {
+        this(year, speed, weight);
+        this.color = color;
+    }
+}
---
+++ basic-2/src/task_4/Main.java
@@ -0,0 +1,11 @@
+package task_4;
+
+public class Main {
+    public static void main(String[] args) {
+        Car carZero = new Car();
+        Car car1 = new Car(2022);
+        Car car2 = new Car(2021, 200);
+        Car car3 = new Car(2020, 220, 1600);
+        Car carAll = new Car(2019, 180, 2000, "green");
+    }
+}
