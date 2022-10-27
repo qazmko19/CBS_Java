d96485f475b6fed7790e12fbc634d2eae8e46ebc
qazmko10 <nikitatslnk@gmail.com>
2022-10-27 19:21:41 +0300

    final commit

---
+++ basic-2/src/task_5/Main.java
@@ -0,0 +1,7 @@
+package task_5;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(MyArea.areaOfCircle(5));
+    }
+}
---
+++ basic-2/src/task_5/MyArea.java
@@ -0,0 +1,10 @@
+package task_5;
+
+public class MyArea {
+    static double pi = 3.14;
+
+
+    public static double areaOfCircle(double radius) {
+        return pi * Math.pow(radius, 2);
+    }
+}
