6c8865a7b535e74c560de088109c8c972ac09503
qazmko10 <nikitatslnk@gmail.com>
2022-10-27 18:47:51 +0300

    final commit

---
+++ basic-2/src/task_2/Car.java
@@ -0,0 +1,21 @@
+package task_2;
+
+public class Car {
+    int year;
+    String color;
+
+    public Car() {}
+
+    public Car(int year) {
+        this.year = year;
+    }
+
+    public Car(String color) {
+        this.color = color;
+    }
+
+    public Car(int year, String color) {
+        this.year = year;
+        this.color = color;
+    }
+}
---
+++ basic-2/src/task_2/Main.java
@@ -0,0 +1,10 @@
+package task_2;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car0 = new Car();
+        Car carColor = new Car("white");
+        Car carYear = new Car(2001);
+        Car car = new Car(1999, "Green");
+    }
+}
